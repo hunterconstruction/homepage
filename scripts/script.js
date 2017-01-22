33e8a7ffac1b8b211e04285d959c138c0a2f7528
Jelique <okieyoshighhost@gmail.com>
2017-01-22 15:23:09 -0500

    1.0 Initial

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+
+			$('.heroHeader-slider').unslider({
+				autoplay: true,
+				infinite: true,
+				delay: 5000,
+				speed: 1000,
+			});
+
+
+			var slider = $('.heroheaderslider').unslider();
+});
